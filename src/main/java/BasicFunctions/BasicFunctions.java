b4a23ffcbd773e8bc9a3c9fde6a7c7c6e74a5876
jackson <jackson@paladincloud.io>
2024-06-17 14:03:20 -0700

    basic functions to test tools with

---
+++ src/main/java/BasicFunctions/BasicFunctions.java
@@ -0,0 +1,23 @@
+package BasicFunctions;
+
+public class BasicFunctions {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public int[] sortArray(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (array[j] > array[j+1]) {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+}
